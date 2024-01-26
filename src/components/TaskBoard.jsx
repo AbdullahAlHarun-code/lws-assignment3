3e8b5240468dbbaa304d2c0aafbd262a618ae545
Abdullah Al Harun <aharun46@gmail.com>
2024-01-26 02:31:46 +0000

    create file and folder and breakdown all html file and convert to react app

---
+++ src/components/TaskBoard.jsx
@@ -0,0 +1,17 @@
+import TasksList from "./TasksList";
+import TasksManage from "./TasksManage";
+
+export default function TaskBoard() {
+  return (
+    <section className="mb-20" id="tasks">
+      <div className="container">
+        <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+          
+          <TasksManage />
+
+          <TasksList />
+        </div>
+      </div>
+    </section>
+  );
+}
